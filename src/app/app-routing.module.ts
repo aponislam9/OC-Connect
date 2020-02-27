65e2243be42f94445138462ad241831296c2d4e5
pasuco-podelo <56372512+pasuco-podelo@users.noreply.github.com>
2020-02-27 09:59:46 -0800

    Adding Event View Page (Non-Functional Prototype)
    This page is used for users to see the full event details but also interact with other users via a forum. Interacting with the page doesn't do anything yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
   {
     path: 'create-event',
     loadChildren: () => import('./create-event/create-event.module').then( m => m.CreateEventPageModule)
+  },  {
+    path: 'event-view',
+    loadChildren: () => import('./event-view/event-view.module').then( m => m.EventViewPageModule)
   }
+
 ];
 @NgModule({
   imports: [
---
+++ src/app/event-view/event-view.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-view',
+  templateUrl: './event-view.page.html',
+  styleUrls: ['./event-view.page.scss'],
+})
+export class EventViewPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
