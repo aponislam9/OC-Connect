5898a8bdfc7631bc37c73bad6b1a90c7646ab1a9
pasuco-podelo <56372512+pasuco-podelo@users.noreply.github.com>
2020-03-04 11:17:55 -0800

    Added more CSS styling to event-view
    Added CSS to make event-view look better. Also added minor changes to ts files, this will change later once backend is finalized.

--- src/app/event-view/event-view.page.ts
+++ src/app/event-view/event-view.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-view',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventViewPage implements OnInit {
 
-  clickBackButton(){
-    
+  constructor(private router: Router) {}
+
+  handleCommentClick() {
+    this.router.navigateByUrl('/comment-view');
   }
+
 
   eventDetails = {
     title: "1 Million Cups: Wing Pitch",
@@ -25,14 +29,35 @@ export class EventViewPage implements OnInit {
   //   slidesPerView: 3
   // }
 
+
+  user01 = {
+    name: "Michael Johnson",
+    picture: "assets/img/stock_1.jpg",
+    commentTime: "Feb, 23 at 8:34 AM",
+    commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.",
+  }
+  user02 = {
+    name: "Jordan Stevens",
+    picture: "assets/img/stock_2.jpg",
+    commentTime: "Feb, 23 at 8:35 AM",
+    commentText: "Elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit, sed do eiusmod tempor",
+  }
+  
+  user03 = {
+    name: "Kyle McKenen",
+    picture: "assets/img/stock_3.jpg",
+    commentTime: "Feb, 23 at 8:37 AM",
+    commentText: "Ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.",
+  }
+
+  userSubComments = [this.user01, this.user02, this.user03]
+
   user1 = {
     name: "Michael Johnson",
     picture: "assets/img/stock_1.jpg",
     commentTime: "Feb, 23 at 8:34 AM",
     commentText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.",
-    sub_comments: ["Lorem ipsum dolor sit amet, consectetur",
-                   "eiusmod tempor incididunt ut labore",
-                   "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud"],
+    sub_comments: this.userSubComments,
     subCommentCount: 3
   }
   user2 = {
@@ -55,8 +80,6 @@ export class EventViewPage implements OnInit {
 
   userComments = [this.user1, this.user2, this.user3]
 
-
-  constructor() {}
 
   ngOnInit() {
   }
