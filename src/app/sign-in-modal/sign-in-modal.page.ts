2e6a482850a86160462fcfe0e8e6600bda779231
Ranim Chaaban <rchaaban@users.noreply.github.com>
2020-03-12 12:49:21 -0700

    got signin modal to work with dummy data

--- src/app/sign-in-modal/sign-in-modal.page.ts
+++ src/app/sign-in-modal/sign-in-modal.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ModalController } from "@ionic/angular";
 import { Storage } from "@ionic/storage";
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: "app-sign-in-modal",
@@ -12,7 +13,8 @@ export class SignInModalPage implements OnInit {
   enteredpword;
   constructor(
     private modalController: ModalController,
-    private storage: Storage
+    private storage: Storage,
+    private alertCtrl: AlertController
   ) {}
 
   public userTest = {
@@ -28,22 +30,56 @@ export class SignInModalPage implements OnInit {
   }
 
   updateSignInDB() {
-    this.storage.get("signed_in_user").then(users_in => {
-      console.log("all signed in users");
-      console.log(users_in);
-      if (users_in != null) {
-        console.log("singned in users is not null");
-        console.log(users_in);
-        users_in.add(this.enteredemail);
-        this.storage.set("signed_in_user", users_in);
-        console.log(users_in);
-      } else {
-        console.log("signed in users is null");
-        this.storage.set("signed_in_user", []);
+    this.storage.get("all_users").then(users => {
+      if (users != null) {
+        console.log("not null");
+        if (users.length > 0) {
+          console.log("bigger 0");
+          for (let u of users) {
+            if (
+              u.email == this.enteredemail &&
+              u.password == this.enteredpword
+            ) {
+              console.log("adding");
+              this.storage.set("signed_in_user", u.name);
+              this.storage.get("signed_in_user").then(val => {
+                console.log(val);
+              });
+              this.modalController.dismiss();
+            } else {
+              console.log("not adding");
+              this.tryAgain();
+              this.enteredemail = "";
+              this.enteredpword = "";
+            }
+          }
+        }
       }
     });
+    // this.storage.get("signed_in_user").then(users_in => {
+    //   console.log("all signed in users");
+    //   console.log(users_in);
+    //   if (users_in != null) {
+    //     console.log("singned in users is not null");
+    //     console.log(users_in);
+    //     users_in.add(this.enteredemail);
+    //     this.storage.set("signed_in_user", users_in);
+    //     console.log(users_in);
+    //   } else {
+    //     console.log("signed in users is null");
+    //     this.storage.set("signed_in_user", []);
+    //   }
+    // });
 
-    this.modalController.dismiss();
+    //this.modalController.dismiss();
+  }
+  async tryAgain() {
+    const alert = await this.alertCtrl.create({
+      header: "Invalid",
+      message: "Try a valid email and/or password",
+      buttons: ["Okay"]
+    });
+    await alert.present();
   }
 
   updateUserDB() {
@@ -75,6 +111,10 @@ export class SignInModalPage implements OnInit {
 
   ngOnInit() {
     //this.updateSignInDB();
-    this.updateUserDB();
+    //this.updateUserDB();
+    //this.storage.clear();
+    let l = [];
+    l.push(this.userTest);
+    this.storage.set("all_users", l);
   }
 }
